cf10ec8942e058d2c719a993a529d97af75fbd60
gabriela03-9 <gabrielatipanluisa03@gmail.com>
2025-01-09 12:02:22 -0500

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.login;
+
+import com.mycompany.login.igu.Principal;
+import javax.swing.JOptionPane;
+
+
+
+
+public class Login {
+
+    public static void main(String[] args) {
+        Principal princ =new Principal();
+  princ.setVisible(true);
+  princ.setLocationRelativeTo(null);
+    }
+     
+      
+}
